c58878daaa1636dd66a4d16900c5647852150f87
eyadjaloudi <59662107+eyadjaloudi@users.noreply.github.com>
2020-01-08 20:01:51 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -21,7 +21,10 @@ public class Test
         System.out.println (p.getName ());		
 
         Person p= new Person ("Lobo", 33);
-        System.out.println (p.getName ());		
+        System.out.println (p.getName ());
+	
+	    
+	System.out.println ("eyad jaludi 20170450");    
 
     }
 }
